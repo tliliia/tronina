1410b552d93bf02fb909c4ca9660f1fa01c7a3b3
troninalily <lily.kmtd@gmail.com>
2021-02-17 12:26:01 +0400

    SolutionBinaryGap

--- main.go
+++ main.go
@@ -10,23 +10,34 @@ func main() {
 	fmt.Println(squares)
 }
 
-//SquarePow использует стандартную библиотеку
-func SquarePow(n float64) int {
-	c := math.Pow(n, 2)
-	return int(c)
-}
-
-//SolutionSquareGenerator is a func
+//SolutionSquareGenerator is a func that uses math/pow
 func SolutionSquareGenerator(start int, n int) []int {
 	var squaresArray = make([]int, n)
 	fmt.Println(squaresArray)
 	for i := range squaresArray {
-		funcN := float64(start + i)
-		squaresArray[i] = int(SquarePow(funcN))
+		floatSquare := math.Pow(float64(start+i), 2)
+		squaresArray[i] = int(floatSquare)
 	}
 	return squaresArray
 }
 
+//SolutionBinaryGap is a func
 func SolutionBinaryGap(N int) int {
-	return 0
+	var maxGap int
+	var currentGap int
+	for N != 0 {
+		lastBit := N % 2
+		fmt.Println(lastBit)
+		if lastBit == 0 {
+			currentGap++
+		} else {
+			currentGap = 0
+		}
+
+		if currentGap > maxGap {
+			maxGap = currentGap
+		}
+		N = int(N / 2)
+	}
+	return maxGap
 }
